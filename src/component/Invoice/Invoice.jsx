85b56dc2d5127468ca9cdec8768fe9417494a6ae
Muhammed Safvan <103893917+safvanms@users.noreply.github.com>
2023-08-18 21:41:24 +0530

    Update Invoice.jsx

--- src/component/Invoice/Invoice.jsx
+++ src/component/Invoice/Invoice.jsx
@@ -51,9 +51,21 @@ export default function Invoice() {
   }
 
   const formatTimestamp = (timestamp) => {
-    const dateObject = new Date(timestamp)
-    return dateObject.toLocaleString()
-  }
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dateObject = new Date(timestamp);
+
+    const dayOfWeek = daysOfWeek[dateObject.getDay()];
+    const day = dateObject.getDate().toString().padStart(2, '0');
+    const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+    const year = dateObject.getFullYear();
+    const hours = dateObject.getHours().toString().padStart(2, '0');
+    const minutes = dateObject.getMinutes().toString().padStart(2, '0');
+    const seconds = dateObject.getSeconds().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    return `${dayOfWeek}, ${day}/${month}/${year}, ${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
 
   const clearData = () => {
     const confirmClear = window.confirm(
